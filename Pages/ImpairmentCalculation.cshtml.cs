2c4a8c2a1aa0ac199621cff0666233d513328715
Wojciech Wosik <wojciech@wosik.pl>
2019-09-27 14:07:55 +0200

    Add project files.

---
+++ Pages/ImpairmentCalculation.cshtml.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace FlorianTabs.Pages
+{
+    public class ImpairmentCalculationModel : PageModel
+    {
+    
+        public void OnGet()
+        {
+
+        }
+    }
+}
